455b7fe74664fea71f986a17c4d60168ae24d55f
sudo-charan <charanaikofficial@gmail.com>
2026-09-05 22:51:50 +0530

    mirage.js removed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,7 @@ import './Loader.css'
 export default function Loader({ size = 48, className = '', label = 'Loading...' }) {
   return (
     <div className={`loader ${className}`}>
-      <div style={{ width: size, height: size }}>
-        <l-mirage size={size} speed="2.5" color="black"></l-mirage>
-      </div>
+      <div style={{ width: size, height: size }} />
       {label && <p className="loader-label">{label}</p>}
     </div>
   )
