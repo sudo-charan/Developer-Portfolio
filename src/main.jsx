08f32ed27bcc2cec5f5c546c844afefcc4c57e14
sudo-charan <charanaikofficial@gmail.com>
2026-09-13 19:00:12 +0530

    feat: improve admin panel, fix bugs, and add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
 import ErrorBoundary from './components/ErrorBoundary'
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <ErrorBoundary>
           <App />
+          <Analytics />
         </ErrorBoundary>
       </ThemeProvider>
     </BrowserRouter>
